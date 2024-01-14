fcbfc50903e01128e70f9ed482fe102491aec580
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-14 23:52:50 +0900

    Determine exit code with command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::env;
+use std::process;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: rucco <exit_code>");
+        process::exit(1);
+    }
+
+    let code = &args[1];
+
     println!(".global main");
     println!("main:");
     println!(" mov x8, #93"); // setup exit syscall
-    println!(" mov x0, #42"); // exit code is 42
+    println!(" mov x0, #{}", code); // exit code is 42
     println!(" svc #0"); // invoke syscall
 }
