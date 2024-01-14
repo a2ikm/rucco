1227e892c04b652be54107c1a9a9fe81402595aa
Masato Ikeda <masato.ikeda@gmail.com>
2024-01-14 23:20:48 +0900

    Implement exit with status 42

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!(".global main");
+    println!("main:");
+    println!(" mov x8, #93"); // setup exit syscall
+    println!(" mov x0, #42"); // exit code is 42
+    println!(" svc #0"); // invoke syscall
 }
